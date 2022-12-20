b252957bb325d4f6f06a456d68ba81a667e7d2a9
Tornike Oragvelidze <toel@Tornikes-MacBook-Air.local>
2022-12-20 20:37:53 +0400

    Done

--- project.cpp
+++ project.cpp
@@ -11,6 +11,10 @@ class Market{
         static int ProductCount;
         static int SoldProductCount;
 
+    Market(){
+
+    }    
+
     Market(string ProductName, string Manufacturer, float ProductPrice){
         this->ProductName=ProductName;
         this->Manufacturer=Manufacturer;
@@ -28,7 +32,57 @@ class Market{
         this->ProductName=ProductName;
     }
 
-    void productSell(){
+ 
+
+
+};
+
+int Market::ProductCount=0;
+int Market::SoldProductCount=0;
+
+class Sante: public Market{
+    public:
+        Sante(){
+
+        }
+
+        Sante(string ProductName, float ProductPrice): Market(ProductName, "Sante", ProductPrice) {}
+
+    void productDistribution(){
+        cout<<"Sante's product has been distributed to market.";
+    }
+};
+
+class Pringles: public Market{
+    public:
+        Pringles(){
+
+        }
+
+        Pringles(string ProductName, float ProductPrice): Market(ProductName, "Pringles", ProductPrice) {}
+
+    void productDistribution(){
+        cout<<"Prignles's product has been distributed to market.";
+    }
+};
+
+class Borjomi: public Market{
+    public:
+        Borjomi(){
+            
+        }
+
+        Borjomi(string ProductName, float ProductPrice): Market(ProductName, "Borjomi", ProductPrice) {}
+
+    void productDistribution(){
+        cout<<"Borjomi's product has been distributed to market.";
+    }
+};
+
+class Shopping: public Market{
+    public:
+
+       void productSell(){
         cout<<"The product is sold.";
         SoldProductCount++;
     }
@@ -47,37 +101,17 @@ class Market{
         cout<<product.Manufacturer<<"'s product "<<product.getProductName()<<" was sold to customer for "<<product.ProductPrice<<" $";
         SoldProductCount++;
     }
-
-
 };
 
-int Market::ProductCount=0;
-int Market::SoldProductCount=0;
+int main(){
+    Sante obj;
+    obj.productDistribution();
+    Sante milk("Milk", 2.99);
+    Borjomi object;
+    object.productDistribution();
+    Borjomi water("Water", 1.99);
+    Pringles objinio;
+    objinio.productDistribution();
+    Pringles chips("Chips", 9.99);
 
-class Sante: public Market{
-    public:
-        Sante(string ProductName, float ProductPrice): Market(ProductName, "Sante", ProductPrice) {}
-
-    void productDistribution(){
-        cout<<"Sante's product has been distributed to market.";
-    }
-};
-
-class Pringles: public Market{
-    public:
-        Pringles(string ProductName, float ProductPrice): Market(ProductName, "Pringles", ProductPrice) {}
-
-    void productDistribution(){
-        cout<<"Prignles's product has been distributed to market.";
-    }
-};
-
-class Borjomi: public Market{
-    public:
-        Borjomi(string ProductName, float ProductPrice): Market(ProductName, "Borjomi", ProductPrice) {}
-
-    void productDistribution(){
-        cout<<"Borjomi's product has been distributed to market.";
-    }
-};
-
+}
